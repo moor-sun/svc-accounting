6df274d3e7d7f104dca302956601dce7c7843f6e
moor-sun <n.sundaramoorthy@gmail.com>
2025-12-01 12:14:18 +0530

    Refactor AccountRepository to TransactionRepository

--- src/main/java/com/example/accounting/repository/TransactionRepository.java
+++ src/main/java/com/example/accounting/repository/TransactionRepository.java
@@ -1,10 +1,7 @@
 package com.example.accounting.repository;
 
-import com.example.accounting.model.Account;
+import com.example.accounting.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> findByAccountNumber(String accountNumber);
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
